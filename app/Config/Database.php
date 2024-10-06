66651bd720351bd59147cf27b8e66403731f46d1
rezafikkri <fikkri.reza@gmail.com>
2024-10-06 15:16:29 +0700

    feat: database connection

---
+++ app/Config/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace RezaFikkri\PLM\Config;
+
+use PDO;
+
+class Database
+{
+    private static ?PDO $dbc = null;
+
+    public static function getConnection(): PDO
+    {
+        // implement singleton design pattern
+        if (is_null(self::$dbc)) {
+            // create new database connection
+            self::$dbc = new PDO(
+                dsn: "pgsql:host=$_ENV[DB_HOST];port=$_ENV[DB_PORT];dbname=$_ENV[DB_NAME]",
+                username: $_ENV['DB_USER'],
+                password: $_ENV['DB_PASSWORD'],
+            );
+        }
+        return self::$dbc;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,9 +2,13 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Dotenv\Dotenv;
 use RezaFikkri\PLM\Middleware\AuthMiddleware;
 use RezaFikkri\PLM\App\Router;
 use RezaFikkri\PLM\Controller\{HomeController, ProductController};
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
 Router::add('GET', '/', HomeController::class, 'index');
 Router::add('GET', '/hello', HomeController::class, 'hello', [AuthMiddleware::class]);
