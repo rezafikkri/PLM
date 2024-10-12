0191bc3cc3fd1d72dd7ecad1acf48155f37a08d6
rezafikkri <fikkri.reza@gmail.com>
2024-10-12 10:54:17 +0700

    feat: improve home page design

--- app/View/Home/index.php
+++ app/View/Home/index.php
@@ -1,9 +1,9 @@
 <main class="form-signin w-100 mx-auto mt-6">
-    <h1 class="h3 mb-2 fw-bold">Login</h1>
+    <h1 class="h2 mb-2 fw-bold">Login</h1>
     <p class="mb-4 text-body-secondary">PHP Login Management System</p>
 
-    <a href="/register" class="btn btn-primary w-100 mb-2.5 py-2">Register</a>
-    <a href="/login" class="btn btn-primary w-100 py-2">Login</a>
+    <a href="/register" class="btn btn-lg btn-primary w-100 mb-2.5 fw-medium">Register</a>
+    <a href="/login" class="btn btn-lg btn-primary w-100 fw-medium">Login</a>
 
-    <p class="mt-4 mb-5 fs-7">By <a href="https://rezafikkri.github.io" class="text-decoration-none">Reza Sariful Fikri</a></p>
+    <p class="mt-4 mb-5 text-body-secondary">By <a href="https://rezafikkri.github.io" class="text-decoration-none">Reza Sariful Fikri</a></p>
 </main>
