94386de5d2b9dc9070e926e7b0e8ea8aeced0394
rezafikkri <fikkri.reza@gmail.com>
2024-10-12 15:31:26 +0700

    test: improve View Template unit testing

--- tests/App/ViewTest.php
+++ tests/App/ViewTest.php
@@ -2,22 +2,30 @@
 
 namespace RezaFikkri\PLM\App;
 
-use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 class ViewTest extends TestCase
 {
-    #[Test]
-    public function render(): void
+    public function testRender(): void
     {
         View::render('Home/index', [
             'title' => 'PHP Login Management',
         ]);
 
-        $this->expectOutputRegex('#PHP Login Management#');
-        $this->expectOutputRegex('#html#');
-        $this->expectOutputRegex('#body#');
-        $this->expectOutputRegex('#Register#');
-        $this->expectOutputRegex('#Login#');
+        // var_dump(preg_match(pattern: '#(?=.*viewport)(?=.*hahaha)#', subject: 'gege hahaha viewport [][][] hehehe', matches: $variables));
+        // var_dump($variables);
+
+        // Menggunakan lookahead assertion (?=...)
+        $this->expectOutputRegex('#(?=.*viewport)(?=.*Register)(?=.*Login)(?=.*<script)#s');
+
+        // note: jika kamu memanggil expectOutputRegex lebih dari 1 kali, maka
+        // value dari regex pattern yang telah di set, akan di replace oleh value
+        // regex pattern dibawahnya. Ex:
+        //
+        // expectOutputRegex('#pertama#')
+        // expectOutputRegex('#kedua#')
+        //
+        // maka regex pattern #edua# akan me-replace regex pattern #pertama#,
+        // jadinya yang akan di cek hanya regex pattern kedua.
     }
 }
