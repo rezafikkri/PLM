a11740a90c5e593325b8c67805cb0df2e2d45adb
rezafikkri <fikkri.reza@gmail.com>
2024-10-14 22:09:04 +0700

    feat: apply Squentially in user registration validation

--- app/Service/UserService.php
+++ app/Service/UserService.php
@@ -10,6 +10,7 @@ use RezaFikkri\PLM\Validator\IsUnique;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\PasswordStrength;
+use Symfony\Component\Validator\Constraints\Sequentially;
 use Symfony\Component\Validator\Validation;
 
 class UserService
@@ -38,8 +39,8 @@ class UserService
     {
         // validate username
         $validator = Validation::createValidator();
-        $violations = [];
-        $userViolations = $validator->validate($request->getUsername(), [
+        $violations = []; 
+        $userViolations = $validator->validate($request->getUsername(), new Sequentially([
             new NotBlank([
                 'message' => 'Username should not be blank.'
             ]),
@@ -48,18 +49,18 @@ class UserService
                 'minMessage' => 'Username is too short. It should have {{ limit }} characters or more.',
             ]),
             new IsUnique('users', 'username'),
-        ]);
+        ]));
         if (count($userViolations) > 0) {
             $violations[] = $userViolations[0]->getMessage();
         }
 
         // validate password
-        $passwordViolations = $validator->validate($request->getPassword(), [
+        $passwordViolations = $validator->validate($request->getPassword(), new Sequentially([
             new NotBlank([
                 'message' => 'Password should not be blank.',
             ]),
             new PasswordStrength(),
-        ]);
+        ]));
         if (count($passwordViolations) > 0) {
             $violations[] = $passwordViolations[0]->getMessage();
         }
