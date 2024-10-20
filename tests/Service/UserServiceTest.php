37e4514b0236dfc552efed1ffb67c33908c32d59
rezafikkri <fikkri.reza@gmail.com>
2024-10-20 09:34:53 +0700

    test: User Login Service

--- tests/Service/UserServiceTest.php
+++ tests/Service/UserServiceTest.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\TestCase;
 use RezaFikkri\PLM\Config\Database;
 use RezaFikkri\PLM\Entity\User;
 use RezaFikkri\PLM\Exception\ValidationException;
+use RezaFikkri\PLM\Model\UserLoginRequest;
 use RezaFikkri\PLM\Model\UserRegisterRequest;
 use RezaFikkri\PLM\Repository\UserRepository;
 
@@ -65,5 +66,54 @@ class UserServiceTest extends TestCase
         $request->setPassword('eddkhjrzafikrid&');
 
         $this->userService->register($request);
+    }
+
+    #[Test]
+    public function loginNotFound(): void
+    {
+        $this->expectException(ValidationException::class);
+
+        $request = new UserLoginRequest;
+        $request->setUsername('rezafikkri');
+        $request->setPassword('rezafikkri');
+
+        $this->userService->login($request);
+    }
+
+    #[Test]
+    public function loginWrongPassword(): void
+    {
+        $user = new User;
+        $user->setUsername('rezafikkri');
+        $user->setPassword(password_hash('password12345', PASSWORD_BCRYPT));
+
+        $this->userRepository->save($user);
+
+        $this->expectException(ValidationException::class);
+
+        $request = new UserLoginRequest;
+        $request->setUsername('rezafikkri');
+        $request->setPassword('password12');
+
+        $this->userService->login($request);
+    }
+
+    #[Test]
+    public function loginSuccess(): void
+    {
+        $user = new User;
+        $user->setUsername('rezafikkri');
+        $user->setPassword(password_hash('password12345', PASSWORD_BCRYPT));
+
+        $this->userRepository->save($user);
+
+        $request = new UserLoginRequest;
+        $request->setUsername('rezafikkri');
+        $request->setPassword('password12345');
+
+        $response = $this->userService->login($request);       
+
+        $this->assertEquals($request->getUsername(), $response->getUser()->getUsername());
+        $this->assertTrue(password_verify($request->getPassword(), $response->getUser()->getPassword()));
     }
 }
