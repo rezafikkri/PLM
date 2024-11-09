bd0aa9c78e2a3e3a1bcf5d852968980c1edc8e2b
rezafikkri <fikkri.reza@gmail.com>
2024-11-09 14:43:44 +0700

    feat: update footer in home page

--- app/View/Home/index.php
+++ app/View/Home/index.php
@@ -5,5 +5,5 @@
     <a href="/users/register" class="btn btn-lg btn-primary w-100 mb-2.5 fw-medium">Register</a>
     <a href="/users/login" class="btn btn-lg btn-primary w-100 fw-medium">Login</a>
 
-    <p class="mt-4 mb-5 text-body-secondary">By <a href="https://rezafikkri.github.io" class="text-decoration-none">Reza Sariful Fikri</a></p>
+    <p class="mt-4 mb-5 text-body-secondary">Made by <a href="https://rezafikkri.github.io" class="text-decoration-none">Reza Sariful Fikri</a> with ❤</p>
 </main>
